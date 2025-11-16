e14b99bee7a1b371d5c20533bc8d054957fe330d
Ayush <ayushprasad481@gmail.com>
2025-11-17 03:01:28 +0530

    add split doc for chunkimg doc

--- splitter.py
+++
@@ -1,5 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=80)
-def split_text(text):
-    return text_splitter.split_text(text)
